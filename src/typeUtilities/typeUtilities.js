dd1a5da12f7e0fcbbc78de55967a18beb9f9e730
Aman Kumar <kumaraman140795@gmail.com>
2024-03-14 14:49:00 +0530

    Add implementation for type utilities

---
+++ src/typeUtilities/typeUtilities.js
@@ -0,0 +1,23 @@
+function isBoolean(value) {
+    return typeof value === "boolean";
+}
+
+function isNumber(value) {
+    return typeof value === "number";
+}
+
+function isNull(value) {
+    return value === null;
+}
+
+function isString(value) {
+    return typeof value === "string";
+}
+
+function isSymbol(value) {
+    return typeof value === "symbol";
+}
+
+function isUndefined(value) {
+    return typeof value === "undefined";
+}
